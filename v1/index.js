eee66156cc0b8ff0e30d5de4ec77d053e7ea2290
Agustin Moyano <agustin.moyano@gmail.com>
2016-03-28 17:04:24 -0300

    se agregan endpoints de schema

--- v1/index.js
+++ v1/index.js
@@ -1,8 +1,10 @@
 var mongo = require('mongo-factory');
 var oid = require('mongodb').ObjectID;
 var express = require('express');
+var extend = require('extend');
 var config = require('/etc/nodejs-config/cendraCM').backend;
 var url = 'mongodb://'+config.mongo.host+':'+config.mongo.port+'/'+config.mongo.db;
+var Promise = require('promise');
 
 module.exports = function() {
   var version = express.Router();
@@ -11,11 +13,88 @@ module.exports = function() {
     var dc = db.collection('documents');
     var sc = db.collection('schemas');
 
+    var isEmptyObject = function(obj) {
+      for(var i in obj) {
+        return false;
+      }
+      return true;
+    }
+
+    var compatibleSchema = function(sch1, sch2) {
+      if(sch1 != sch2) {
+        if(isEmptyObject(sch1)||isEmptyObject(sch2)) return true;
+        if(sch1.type != sch2.type) return false;
+        if(sch1.type == 'object') {
+          for(var i in sch1.properties) {
+            if(sch2.hasOwnProperty(i) && !compatibleSchema(sch1.properties[i], sch2.properties[i])) return false;
+          }
+        }
+        if(sch1.type == 'array') {
+          //Hay por lo menos un items no definido?
+          if(!sch1.items||!sch2.items) return true;
+          //Ambos items son array?
+          if(Array.isArray(sch1.items) && Array.isArray(sch2.items)) {
+            var min = sch1.items.length<sch2.items.length?sch1.items:sch2.items;
+            var max = sch1.items.length<sch2.items.length?sch2.items:sch1.items;
+            var arrayEquals = true;
+            min.forEach(function(item, i) {
+              if(arrayEquals) {
+                arrayEquals = compatibleSchema(item, max[i]);
+              }
+            });
+            return arrayEquals;
+          }
+          //Hay un items que es array y otro object?
+          if(Array.isArray(sch1.items) || Array.isArray(sch2.items)) {
+            var arr = Array.isArray(sch1.items)?sch1.items:sch2.items;
+            var obj = sch2.items==arr?sch1.items:sch2.items;
+            var arrayEquals = true;
+            arr.forEach(function(item, i) {
+              if(arrayEquals) {
+                arrayEquals = compatibleSchema(item, obj);
+              }
+            });
+            return arrayEquals;
+          }
+          //Ambos items son object
+          return compatibleSchema(sch1.items, sch2.items);
+        }
+      }
+      return true;
+    }
+
+    var reduceSchema = function(arr) {
+      return new Promise(function(resolve, reject) {
+        sc.find({'_id': {$in: arr}}).toArray(function(err, schs) {
+          if(err) return reject(err);
+          resolve(schs);
+        });
+      })
+      .then(function(schs) {
+        return schs.reduce(function(memo, item) {
+          return new Promise(function(resolve, reject) {
+            if(!compatibleSchema(memo, item)) return reject('Esquemas no compatibles');
+            resolve(extend(true, memo, item));
+          });
+        }, {});
+      })
+
+    }
+
     version.get('/schema', function(req, res, next) {
       sc.find(req.query).toArray(function(err, schs) {
         if(err) return res.status(500).send(err);
         res.json(schs);
       });
+    });
+    version.get('/schema/reduce', function(req, res, next) {
+      reduceSchema(req.body)
+      .then(function(sch) {
+        res.json(sch);
+      })
+      .catch(function(err) {
+        res.status(500).send(err);
+      })
     });
     version.post('/schema', function(req, res, next) {
       sc.insertOne(req.body)
@@ -24,6 +103,12 @@ module.exports = function() {
       })
       .catch(function(err) {
         res.status(500).send(err);
+      });
+    });
+    version.get('/schema/:id', function(req, res, next) {
+      sc.find({"_id": new oid(req.params.id)}).limit(1).next(function(err, sch) {
+        if(err) return res.status(500).send(err);
+        res.json(sch);
       });
     });
 
